53402ac3112e4430a3d03cdbda014b06d3d8c843
sbayer <bayer@sunsteps.org>
2012-11-07 21:58:41 +0100

    * add test for /director-route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rottenpotatoes::Application.routes.draw do
   #   resources :products
   resources :movies
 
+  match "/director/:director" => "movies#director"
+
   # Sample resource route with options:
   #   resources :products do
   #     member do
